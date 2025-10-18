2ca090b583811fe9ab70af735d3a1f630f8347d3
LUIS EDUARDO RIVERA MARTOS <lriveram@unicauca.edu.co>
2025-10-18 18:12:09 -0500

    middlleword

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import morgan from "morgan"
+
+// Cargar variables de entorno
+dotenv.config();
+
+const server = express()
+const PORT = process.env.PORT || 8080
+const ready = ()=>  console.log(`✅ Servidor corriendo en http://localhost:${PORT}`)
+
+//configuracion global 
+server.use(express.json()); // Permite leer JSON del body
+server.use(cors({ origin: process.env.CLIENT_URL || "*" }))
+server.use(morgan("dev")); // Logs HTTP en consola
+
+// Iniciar el servidor
+server.listen(PORT,ready);
